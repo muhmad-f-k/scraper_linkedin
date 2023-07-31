b7ab1cb81a212591ead3b98b039534f3d8077233
Muhmad K. Fauzi <muhmad@live.no>
2023-08-01 01:39:53 +0200

    Implementation of asynchronous processing techniques, in combination with the ThreadPoolExecutor from Python's concurrent.futures module, has significantly enhanced scraper performance. This approach optimizes resource allocation and reduces execution time through parallel processing of tasks, thereby contributing to the overall robustness and reliability of the scraper solution.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Import necessary modules
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 import os
 import pickle
@@ -11,11 +12,8 @@ import time
 import json
 import random
 
-# Define LinkedIn class
-
 
 class LinkedIn:
-
     LOGIN_URL = "https://www.linkedin.com/login"  # LinkedIn login URL
     # Path to store/load cookies
     COOKIE_PATH = os.path.join(os.getcwd(), 'cookies.pkl')
@@ -23,13 +21,14 @@ class LinkedIn:
     # Class constructor
     def __init__(self):
         load_dotenv()  # Load environment variables
-        # Get LinkedIn username from environment variable
+        # Load LinkedIn username
         self.username = os.getenv('LINKEDIN_USERNAME')
-        # Get LinkedIn password from environment variable
+        # Load LinkedIn password
         self.password = os.getenv('LINKEDIN_PASSWORD')
 
-        # Instantiate Chrome Options object
+        # Initialize Chrome options
         self.chromeOptions = webdriver.ChromeOptions()
+        # Define Chrome preferences to disable unnecessary popups and notifications
         self.prefs = {
             "profile.default_content_setting_values.notifications": 2,
             "profile.managed_default_content_settings.plugins": 1,
@@ -37,100 +36,139 @@ class LinkedIn:
             "profile.managed_default_content_settings.geolocation": 2,
             "profile.managed_default_content_settings.media_stream": 2,
         }
-        self.chromeOptions.add_experimental_option(
-            "prefs", self.prefs)  # Add preferences to Chrome Options
+        # Add preferences to Chrome options
         # self.chromeOptions.add_argument("--headless")  # Run in headless mode
-        self.chromeOptions.add_argument("--no-sandbox")  # Disable sandbox mode
-        # Disable features that might betray automation
+        self.chromeOptions.add_experimental_option("prefs", self.prefs)
+        # Disable sandbox mode for Chrome
+        self.chromeOptions.add_argument("--no-sandbox")
+        # Disable some features to make automation less detectable
         self.chromeOptions.add_argument("--disable-blink-features")
-        # Disable 'automation' toolbar
         self.chromeOptions.add_argument(
             "--disable-blink-features=AutomationControlled")
         self.chromeOptions.add_argument("--disable-dev-shm-usage")
-        self.chromeOptions.add_argument("disable-infobars")  # Disable infobars
+        # Disable infobars
+        self.chromeOptions.add_argument("disable-infobars")
+        # Set user agent to look like a regular browser
         self.chromeOptions.add_argument(
-            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')  # Set user agent
-        self.driver = webdriver.Chrome(
-            options=self.chromeOptions)  # Initiate Chrome WebDriver
+            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
+        # Initialize Chrome WebDriver with the defined options
+        self.driver = webdriver.Chrome(options=self.chromeOptions)
 
-    # Method to load cookies
+    # Method to load cookies from file
     def load_cookies(self):
         try:
-            with open(self.COOKIE_PATH, 'rb') as f:  # Open the file to read in binary mode
-                cookies = pickle.load(f)  # Load cookies from file
-            for cookie in cookies:  # Iterate over each cookie
-                self.driver.add_cookie(cookie)  # Add cookie to WebDriver
+            # Try to open the cookie file
+            with open(self.COOKIE_PATH, 'rb') as f:
+                # Load cookies from the file
+                cookies = pickle.load(f)
+            # Add each cookie to the WebDriver
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
             print('Cookies loaded')
-        except FileNotFoundError:  # Exception handling for file not found error
+        except FileNotFoundError:
             print('No cookies found')
             return False
 
         return True
 
-    # Method to save cookies
+    # Method to save cookies to file
     def save_cookies(self):
-        cookies = self.driver.get_cookies()  # Get cookies from WebDriver
-        with open(self.COOKIE_PATH, 'wb') as f:  # Open the file to write in binary mode
-            pickle.dump(cookies, f)  # Dump cookies into file
+        # Get cookies from WebDriver
+        cookies = self.driver.get_cookies()
+        # Open the cookie file to write
+        with open(self.COOKIE_PATH, 'wb') as f:
+            # Save cookies to the file
+            pickle.dump(cookies, f)
         print('Cookies saved')
 
-    # Method to perform login action
-    def login(self):
-        self.driver.get(self.LOGIN_URL)  # Open LinkedIn login page
+    # Asynchronous method to login
+    async def login(self):
+        loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor() as executor:
+            # Run the synchronous login implementation in a separate thread
+            await loop.run_in_executor(executor, self._login_impl)
 
-        if not self.load_cookies():  # If no cookies are loaded, perform login operation manually
+    # Synchronous method to login
+    def _login_impl(self):
+        # Open the LinkedIn login page
+        self.driver.get(self.LOGIN_URL)
+        # Try to load cookies
+        if not self.load_cookies():
+            # If cookies can't be loaded, manually enter the username and password
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
-                (By.ID, "username"))).send_keys(self.username)  # Wait until username field is present and input username
+                (By.ID, "username"))).send_keys(self.username)
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
-                (By.ID, "password"))).send_keys(self.password)  # Wait until password field is present and input password
+                (By.ID, "password"))).send_keys(self.password)
             WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
-                (By.XPATH, '//button[@class="btn__primary--large from__button--floating"]'))).click()  # Wait until login button is clickable and click
-            self.save_cookies()  # Save cookies after successful login
+                (By.XPATH, '//button[@class="btn__primary--large from__button--floating"]'))).click()
+            # Save the cookies after logging in
+            self.save_cookies()
 
-    # Extract text from a specified URL
-    def extract_text(self):
-        # Load keywords from json file
+    # Asynchronous method to extract text
+    async def extract_text(self):
+        loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor() as executor:
+            # Run the synchronous text extraction implementation in a separate thread
+            await loop.run_in_executor(executor, self._extract_text_impl)
+
+    # Synchronous method to extract text
+    def _extract_text_impl(self):
+        # Open the keywords.json file
         with open('keywords.json', 'r', encoding='utf-8') as f:
+            # Load keywords from the file
             data = json.load(f)
             keywords = data['keywords']
 
         # Choose a random keyword
         keyword = random.choice(keywords)
-
-        # Construct the URL
+        # Construct the search URL
         url = f'https://www.linkedin.com/search/results/content/?keywords={keyword}&sortBy=%22date_posted%22'
+        # Open the search URL
+        self.driver.get(url)
+        SCROLL_PAUSE_TIME = 2
+        scroll_count = 0
 
-        self.driver.get(url)  # Open the specified URL
-        SCROLL_PAUSE_TIME = 2  # Time to pause between scrolls
-        scroll_count = 0  # Initialize scroll counter
-
-        while scroll_count < 7:  # Scroll the page 7 times
+        # Scroll the page to load the search results
+        while scroll_count < 7:
             self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")  # Scroll to bottom of page
-            time.sleep(SCROLL_PAUSE_TIME)  # Wait for page to load
-            scroll_count += 1  # Increment scroll counter
+                "window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
+            scroll_count += 1
 
-        # After the complete page is loaded, then find the elements
+        # Get the text elements from the search results
         elements = self.driver.find_elements(
             By.CSS_SELECTOR, 'div > div.feed-shared-update-v2__description-wrapper.mr2 > div.feed-shared-inline-show-more-text.feed-shared-update-v2__description.feed-shared-inline-show-more-text--minimal-padding > div > span > span > span')
-        for i, element in enumerate(elements):  # Iterate over each element
-            print(element.text)  # Print the text of the element
+        for i, element in enumerate(elements):
+            # Print each text element - Just for testing purposes
+            print(element.text)
             print("___________")
             print(i)
 
-    # Method to quit WebDriver
-    def quit(self):
+    # Asynchronous method to quit WebDriver
+    async def quit(self):
+        loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor() as executor:
+            # Run the synchronous quit implementation in a separate thread
+            await loop.run_in_executor(executor, self._quit_impl)
+
+    # Synchronous method to quit WebDriver
+    def _quit_impl(self):
         print("Browser session ended")
-        self.driver.quit()  # Quit the WebDriver session
+        self.driver.quit()
 
 
 # Instantiate LinkedIn class
 linkedin = LinkedIn()
 
-# Enclose in try-finally block to ensure cleanup even if exception occurs
+# Get the current event loop
+loop = asyncio.get_event_loop()
 try:
-    linkedin.login()  # Perform login action
-    linkedin.extract_text()  # Extract text from specified URL
-    time.sleep(20)  # Pause execution for 20 seconds
+    # Run the login method
+    loop.run_until_complete(linkedin.login())
+    # Run the text extraction method
+    loop.run_until_complete(linkedin.extract_text())
+    # Wait for 20 seconds
+    time.sleep(20)
 finally:
-    linkedin.quit()  # Quit WebDriver session
+    # Always quit the WebDriver to clean up
+    loop.run_until_complete(linkedin.quit())
